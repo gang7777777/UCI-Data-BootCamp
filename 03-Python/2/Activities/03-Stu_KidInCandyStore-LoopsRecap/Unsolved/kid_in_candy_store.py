6a8282c51e8c96707ddc9d0a6b3788d5c112a973
gang7777777 <ronaldfelixalfelor1980@gmail.com>
2023-04-11 23:58:52 -0700

    new class

--- 03-Python/2/Activities/03-Stu_KidInCandyStore-LoopsRecap/Unsolved/kid_in_candy_store.py
+++ 03-Python/2/Activities/03-Stu_KidInCandyStore-LoopsRecap/Unsolved/kid_in_candy_store.py
@@ -1,10 +1,9 @@
 # The list of candies to print to the screen
 candy_list = ["Snickers", "Kit Kat", "Sour Patch Kids", "Juicy Fruit", "Swedish Fish", "Skittles", "Hershey Bar", "Starbursts", "M&Ms"]
-for idx, candy in enumerate(candy_list):
-    print(idx, candy)
+
 # The amount of candy the user will be allowed to choose
 allowance = 5
-candy_number = idx(input("Add candy number"))
+
 # The list used to store all of the candies selected inside of
 candy_cart = []
 
--- 03-Python/2/Activities/05-Ins_BasicRead/Unsolved/read_file.py
+++
@@ -1,5 +0,0 @@
-file='../Resources/input.txt'
-
-with open(file, 'r') as text:
-    lines=text.read()
-    print(text)
